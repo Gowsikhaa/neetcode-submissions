ddcb28755125392405e0b09564a2633632f24304
Gowsikhaa <105860142+Gowsikhaa@users.noreply.github.com>
2026-04-25 22:36:12 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> myStack = new Stack<>();
+       for(char c : s.toCharArray()){
+        if(c == '(' || c == '[' || c == '{'){
+            myStack.push(c);
+        }
+        else if(c==')' && !myStack.isEmpty() && myStack.peek() == '(')
+        {
+            myStack.pop();
+        }
+        else if(c==']' && !myStack.isEmpty() && myStack.peek() == '[')
+        {
+            myStack.pop();
+        }
+        else if(c=='}' && !myStack.isEmpty() && myStack.peek() == '{')
+        {
+            myStack.pop();
+        }
+        else{
+            return false;
+        }
+       } 
+       if(myStack.isEmpty()){
+        return true;
+       }
+       return false;
+    }
+}
